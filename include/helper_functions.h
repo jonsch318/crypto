8eec19d498412cc2c3f853c39c8fe2ec39c2429c
NilEis <52820950+NilEis@users.noreply.github.com>
2021-03-25 19:21:23 +0100

    feat: added RSA key generation
    Added a src file and a header for the generation of RSA key pairs.

---
+++ include/helper_functions.h
@@ -0,0 +1,13 @@
+#ifndef HELPER_FUNCTIONS_H_INCLUDED
+#define HELPER_FUNCTIONS_H_INCLUDED
+#include <stdint.h>
+
+/** \brief Tests whether a number is a prime number
+ *
+ * \param p is the number to be tested
+ * \return 0 if the number is a prime number, otherwise -1
+ *
+ */
+int is_prime(uint32_t p);
+
+#endif // HELPER_FUNCTIONS_H_INCLUDED
