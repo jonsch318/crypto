3a1875efd545208883900fc9eac44b08401d021e
NilEis <52820950+NilEis@users.noreply.github.com>
2021-05-01 15:53:40 +0200

    feat: Added key input for Unix

--- src/menu.c
+++ src/menu.c
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #if defined(_WIN32) || defined(_WIN64)
 #include <conio.h>
+#elif defined(__unix__)
+#include <termios.h>
+#include <sys/ioctl.h>
 #endif
 
 extern char *s_color;
@@ -18,11 +21,11 @@ void m_exit(void);
 
 struct entry
 {
-    char title[15]; //Text written on the screen
-    int foreground; //foreground color
-    int background; //background color
-    int s_foreground; //foreground color -> selected
-    int s_background; //background color -> selected
+    char title[15];    //Text written on the screen
+    int foreground;    //foreground color
+    int background;    //background color
+    int s_foreground;  //foreground color -> selected
+    int s_background;  //background color -> selected
     void (*run)(void); //Function to be executed when selected
 };
 
@@ -31,7 +34,7 @@ struct menu
     struct entry entrys[2];
     //I don't think there will be more than 255 entries -> uint8_t 0-255
     uint8_t num_entrys; //Number of entrys
-    uint8_t selected; //Index of selected entry
+    uint8_t selected;   //Index of selected entry
 } m;
 
 void m_main()
@@ -82,6 +85,15 @@ void m_loop()
     {
         key = -1;
         m_draw();
+#ifdef __unix__
+        struct termios term, term_old;
+        tcgetattr(0, &term_old);
+        tcgetattr(0, &term);
+        term.c_lflag &= ~ICANON;
+        term.c_lflag &= ~ECHO;
+        tcsetattr(0, TCSANOW, &term);
+        setbuf(stdin, NULL); //Deactivate buffer
+#endif                       // __unix__
 #if defined(_WIN32) || defined(_WIN64)
         ch = getch();
         switch (ch)
@@ -114,37 +126,44 @@ void m_loop()
             break;
         }
 #elif defined(__unix__)
-        //getchar does not return codes for the arrow keys
         ch = getchar();
         switch (ch)
         {
-        case 0xE0:
-            ch = getchar();
-            switch (ch)
+        case 0x1B:;
+            int bytes;
+            ioctl(0, FIONREAD, &bytes);
+            if (bytes != 0)
             {
-            case 0x4B: //LEFT
-                key = 0;
-                break;
-                key = 1;
-            case 0x48: //UP
-                key = 2;
-                break;
-            case 0x4D: //RIGHT
-                key = 3;
-                break;
-            case 0x50: //DOWN
-                key = 4;
-                break;
+                getchar();
+                ch = getchar();
+                switch (ch)
+                {
+                case 0x44: //LEFT
+                    key = 0;
+                    break;
+                    key = 1;
+                case 0x41: //UP
+                    key = 2;
+                    break;
+                case 0x43: //RIGHT
+                    key = 3;
+                    break;
+                case 0x42: //DOWN
+                    key = 4;
+                    break;
+                }
+            }
+            else
+            {
+                key = 6; //ESCAPE
             }
             break;
-        case 0x0D: //RETURN
+        case 0x0A: //RETURN
         case 0x20: //SPACE
             key = 5;
             break;
-        case 0x1B: //ESCAPE
-            key = 6;
-            break;
         }
+        tcsetattr(0, TCSANOW, &term_old);
 #endif
         switch (key)
         {
@@ -179,7 +198,7 @@ void m_rsa()
 {
     console_clear();
     console_set_color(WHITE, BLACK);
-    c_printf("RSA:\n");
+    printf("RSA:\n");
     uint32_t p = 0;
     uint32_t q = 0;
     uint32_t N = 0;
@@ -187,20 +206,20 @@ void m_rsa()
     uint32_t d = 0;
     do
     {
-        c_printf("Please enter two prime numbers");
+        printf("Please enter two prime numbers");
         console_set_color(GRAY, BLACK);
-        c_printf(" (prime_1 prime_2):\n");
+        printf(" (prime_1 prime_2):\n");
         console_reset_color();
         scanf("%u %u", &p, &q);
         console_clear();
 
     } while (is_prime(p) != 0 || is_prime(q) != 0);
     RSA_get(p, q, &N, &e, &d);
-    c_printf("p: %u\nq: %u\nN: %u\ne: %u\nd: %u\n", p, q, N, e, d);
+    printf("p: %u\nq: %u\nN: %u\ne: %u\nd: %u\n", p, q, N, e, d);
     console_set_color(GREEN, BLACK);
-    c_printf("Public key: {N: %u, e: %u}\n", N, e);
+    printf("Public key: {N: %u, e: %u}\n", N, e);
     console_set_color(RED, BLACK);
-    c_printf("Private key: {N: %u, d: %u}\n", N, d);
+    printf("Private key: {N: %u, d: %u}\n", N, d);
     console_reset_color();
     getchar();
     printf("Press enter to return...");
