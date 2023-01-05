0fd97e20ac3e89e9540f21e6e76accf69f4e1935
Jonas Schneider <jonas.max.schneider@gmail.com>
2023-01-05 10:57:33 +0100

    xor work

--- src/cyphers/xor.rs
+++ src/cyphers/xor.rs
@@ -34,6 +34,11 @@ fn xor_encrypt_ui() -> i32 {
         }
     }
     let outp = xor_encrypt(&line, &key);
+    
+    for c in outp.chars() {
+        print!("{} {}", c as u32, c);
+    }
+
     println!("Encrypted: {}", outp);
     println!("Length: {}", outp.len());
     return 0;
@@ -47,11 +52,43 @@ fn xor_decrypt_ui() -> i32 {
 fn xor_encrypt(s: &str, t: &str) -> String {
     let mut output = String::with_capacity(s.len());
     let mut i = 0;
-    for c in s.chars() {
-        let c2 = t.chars().nth(i).unwrap();
-        let c3 = c as u32 ^ c2 as u32;
-        output.push(char::from_u32(c3).unwrap());
-        i += 1;
+
+    print!("s: ");
+    for s_char in s.chars() {
+        print!("{} ({})", s_char as u32, s_char);
+    }
+
+    print!("\n t:");
+    for t_char in t.chars() {
+        print!("{} ({})", t_char as u32, t_char);
+    }
+
+
+    for s_char in s.chars() {
+        let t_char = t.chars().nth(i).unwrap();
+        let result = (s_char as u32) ^ (t_char as u32);
+        
+        let ascii = to_ascii(result);
+        for c in ascii {
+            output.push(c);
+        }
+
+        i = (i+1) % t.len();
     }
     return output;
+}
+
+fn to_ascii(s: u32) -> [char; 4] {
+
+    let c1 = (s >> 24) as u8;
+    let c2 = (s >> 16) as u8;
+    let c3 = (s >> 8) as u8;
+    let c4 = s as u8;
+
+    let mut out = [0 as char; 4];
+    out[0] = c1 as char;
+    out[1] = c2 as char;
+    out[2] = c3 as char;
+    out[3] = c4 as char;
+    return out;
 }
