8f347ecd81b81ab6ae8abe2523d2bb69e334cb8e
NilEis <52820950+NilEis@users.noreply.github.com>
2024-10-29 00:18:16 +0100

    initial commit for zig rewrite

--- include/ui/ciphers/ui_xor.h
+++
@@ -1,6 +0,0 @@
-#ifndef UI_XOR_H
-#define UI_XOR_H
-
-void m_xor(void);
-
-#endif // UI_XOR_H
--- include/ui/ui_helper_functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef UI_HELPER_FUNCTIONS_H
-#define UI_HELPER_FUNCTIONS_H
-
-void m_is_prime(void);
-void m_prime_get(void);
-
-#endif // UI_HELPER_FUNCTIONS_H
