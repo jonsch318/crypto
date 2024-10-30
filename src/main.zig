66d6629e67897eb8d0b39ef203f2fc1d7d16a79b
NilEis <52820950+NilEis@users.noreply.github.com>
2024-10-30 18:30:47 +0100

    add: math functions for rsa

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
-pub const rsa = @import("cyphers/rsa.zig");
+const math = @import("util/math.zig");
 
 test {
     std.testing.refAllDecls(@This());
+    std.testing.refAllDecls(math);
 }
